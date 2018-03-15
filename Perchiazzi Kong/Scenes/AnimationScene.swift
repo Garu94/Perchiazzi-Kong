6605aa9eced35e5a3f0072daf36cc65dd0b3d7c9
Simone Garuglieri <simone.garuglieri@student.unisi.it>
2018-03-15 14:26:18 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Perchiazzi Kong/Scenes/AnimationScene.swift
@@ -0,0 +1,25 @@
+//
+//  AnimationScene.swift
+//  Perchiazzi Kong
+//
+//  Created by Simone Garuglieri on 15/03/2018.
+//  Copyright © 2018 Simone Garuglieri. All rights reserved.
+//
+
+import SpriteKit
+
+class AnimationScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let imageView = UIImageView()
+        imageView.sizeThatFits(view.frame.size)
+        view.addSubview(imageView)
+    }
+    
+    
+    func runAnimation() {
+        
+        
+        
+    }
+}
